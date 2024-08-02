8348cb952c43d46a60b29fc98846160e462e602f
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2024-08-02 21:07:31 +0700

    Minor change About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,13 +8,17 @@ import { IconSettings2, IconSparkles, IconBrandLinkedin, IconBrandGithub, IconBr
 
 const About = () => {
 	return (
-		<div className='!mt-36 flex flex-col items-center w-[68rem]'>
-			<div className='border py-2 px-4 rounded-full flex flex-row items-center space-x-2'>
-				<IconPointFilled size={20} stroke={1.0} className='text-[#3643FC]'/>
-				<p className='text-lg'>Open to new opportunities</p>
+		<div className='!mt-40 flex w-[68rem] flex-col items-center'>
+			<div className='hover:bg-customwhite hover:text-customblack flex flex-row items-center space-x-4 rounded-full border bg-[#131313] px-6 py-2 transition-all duration-300 ease-in-out border-customgray shadow-lg'>
+				{/* TODO: Make a shining effect, moving from left to right, either the text or the div */}
+				<span className='relative flex h-4 w-4 items-center justify-center'>
+					<span className='absolute inline-flex h-full w-full animate-ping rounded-full bg-blurple opacity-75'></span>
+					<span className='relative inline-flex h-3 w-3 rounded-full bg-blurple'></span>
+				</span>
+				<p className='text-lg'>Open for new opportunities</p>
 			</div>
 			<div className='mt-12 w-full'>
-				<p className='text-center font-instrument text-6xl'>
+				<p className='text-center font-instrument text-6xl bg-gradient-to-br from-customwhite to-[#5c5c5a] bg-clip-text text-transparent'>
 					Abhip is a brilliant <i>fullstack engineer</i>,<br />a formidable <i>forensics CTF player</i>,<br />
 					and a comprehensive <i>problem solver</i>,<br />
 					developing from the lively city of Jakarta,
@@ -52,7 +56,7 @@ const About = () => {
 				<div className='!ml-6 h-0.5 w-28 bg-white opacity-20' />
 			</div>
 			<div className='mt-16 flex h-auto w-full flex-row space-x-6'>
-				<div className='flex h-full w-[40%] flex-col space-y-6 rounded-3xl border border-[#262626] p-6'>
+				<div className='flex h-full w-[40%] flex-col space-y-6 rounded-3xl border border-[#262626] p-6 shadow-lg'>
 					<div className='flex h-[11.5rem] items-center justify-center'>
 						<SliderSkills />
 					</div>
@@ -70,7 +74,7 @@ const About = () => {
 						</p>
 					</div>
 				</div>
-				<div className='flex h-full w-[39.3rem] flex-col items-center space-y-6 rounded-3xl border border-[#262626] p-6'>
+				<div className='flex h-full w-[39.3rem] flex-col items-center space-y-6 rounded-3xl border border-[#262626] p-6 shadow-lg'>
 					<SliderStacks />
 					<SliderTools />
 					<div className='flex w-full flex-col items-start space-y-4'>
