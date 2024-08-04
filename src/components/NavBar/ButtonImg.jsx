2c52ad412395c95fc192ccf5a8578fbcacbc9fd8
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2024-08-05 00:15:22 +0700

    Profile button done

---
+++ src/components/NavBar/ButtonImg.jsx
@@ -0,0 +1,45 @@
+import Laptop from '../../assets/creds/laptop.png';
+import Flag from '../../assets/creds/flag.png';
+import Globe from '../../assets/creds/globe.png';
+import Test from '../../assets/creds/test.png';
+import Indonesia from '../../assets/creds/indonesia.png';
+import PFP from '../../assets/creds/pfp.png';
+const imgs = [Laptop, Flag, Globe, Test, Indonesia];
+
+import { useState } from 'react';
+
+const ButtonImg = ({ img = null, text, link }) => {
+	const [hovered, setHovered] = useState(false);
+	const [counter, setCounter] = useState(0);
+
+	const currentEmoji = imgs[counter];
+
+	const changeEmoji = () => {
+		setHovered(true);
+		setCounter((counter + 1) % imgs.length);
+	};
+
+	return (
+		<a href={link}>
+			<div
+				className='relative flex h-14 w-14 cursor-pointer items-center justify-center rounded-2xl border border-[#262626] transition-all duration-200 ease-in-out hover:bg-blurple hover:bg-opacity-40 active:bg-blurple active:bg-opacity-85'
+				onMouseEnter={() => changeEmoji()}
+				onMouseLeave={() => setHovered(false)}
+			>
+				<img
+					src={currentEmoji}
+					className={`absolute -bottom-5 -right-6 z-20 ${hovered ? '-rotate-[4deg] scale-[50%]' : 'rotate-12 scale-[40%]'} transition-all duration-200 ease-in-out`}
+				/>
+				<div className='h-full w-full overflow-hidden rounded-2xl'>
+					<img
+						src={PFP}
+						className='grayscale filter transition-all duration-200 ease-in-out hover:filter-none'
+					/>
+				</div>
+				<p className=''>{text}</p>
+			</div>
+		</a>
+	);
+};
+
+export default ButtonImg;
