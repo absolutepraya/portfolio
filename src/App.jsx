8a980cda803dd33c91221310a76e60dd8109803a
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2024-08-09 19:58:00 +0700

    Added new bg for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
 import Copyright from './components/Copyright';
 
-import Grid from './assets/bg/3.jpeg';
+import Grid from './assets/bg/1.jpeg';
+import GridMobile from './assets/bg/2.jpeg';
 
 const App = () => {
 	const [desktopView, setDesktopView] = useState(window.innerWidth >= 768);
@@ -26,10 +27,10 @@ const App = () => {
 
 	return (
 		<div className={'relative flex flex-col items-center space-y-32 overflow-hidden bg-[#0d0d0d] font-inter text-[#cccccc]'}>
-			<NavBar desktopView={desktopView}/>
+			<NavBar desktopView={desktopView} />
 			<img
-				src={Grid}
-				className='absolute -top-96 left-1/2 z-0 w-[88rem] -translate-x-1/2 opacity-65'
+				src={desktopView ? Grid : GridMobile}
+				className='absolute left-1/2 z-0 -translate-x-1/2 md:opacity-65 opacity-100 md:-top-96 -top-[44rem] w-[88rem]'
 			/>
 			<About desktopView={desktopView} />
 			<Experience desktopView={desktopView} />
