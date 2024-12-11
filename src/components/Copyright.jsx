da70f6d6ffe86f1086102c67c93db929411c4862
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2024-12-11 21:28:29 +0700

    Update Copyright component icon sizes for improved consistency and adjust text size for better responsiveness

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -18,19 +18,19 @@ const Copyright = () => {
 	}, []);
 
 	return (
-		<div className='flex font-jetbrainsmono w-full flex-col items-center justify-between space-y-2 border-t border-customgray bg-customgray bg-opacity-40 py-4 text-xs md:h-16 md:flex-row md:space-y-0 md:px-32 md:py-0 md:text-sm'>
+		<div className='flex font-jetbrainsmono w-full flex-col items-center justify-between space-y-2 border-t border-customgray bg-customgray bg-opacity-40 py-4 text-xs md:h-16 md:flex-row md:space-y-0 md:px-32 md:py-0 md:text-xs'>
 			<div className='flex flex-row items-center justify-start space-x-2 md:w-1/3'>
-				<IconCopyright size={desktopView ? 18 : 14} />
+				<IconCopyright size={desktopView ? 16 : 14} />
 				<p>2024 Daffa Abhipraya — All Rights Reserved.</p>
 			</div>
 			<div className='flex flex-row items-center justify-center space-x-2 md:w-1/3'>
 				<p>Jakarta, Indonesia</p>
-				<IconClock size={desktopView ? 18 : 14} />
+				<IconClock size={desktopView ? 16 : 14} />
 				<p>{currentTime}</p>
 			</div>
 			<div className='flex flex-row items-center justify-end space-x-2 md:w-1/3'>
 				<p>Designed and built by me</p>
-				<IconBrandGithub size={desktopView ? 18 : 14} />
+				<IconBrandGithub size={desktopView ? 16 : 14} />
 				<a href='https://github.com/absolutepraya/portfolio'>
 					<p className='underline underline-offset-2'>Source code here.</p>
 				</a>
