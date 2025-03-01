f69b1e050e070f955d5c22aba2a098a22284257b
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2025-03-01 15:08:14 +0700

    Refactor App component: simplify props by removing unnecessary desktopView prop and integrate TabletView for better responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,30 @@ import Contact from './components/Contact/Contact';
 import Copyright from './components/Copyright';
 import Achievements from './components/Achievements/Achievements';
 import DesktopView from './lib/DesktopView';
+import TabletView from './lib/TabletView';
 import Grid from './assets/elements/bg1.webp';
 import GridMobile from './assets/elements/bg2.webp';
 
 const App = () => {
 	const desktopView = DesktopView();
+	const tabletView = TabletView();
 
 	return (
 		<div className={'relative flex flex-col items-center space-y-20 overflow-hidden bg-customblack font-inter text-customwhite md:space-y-32'}>
-			<NavBar desktopView={desktopView} />
+			<NavBar />
 			<img
-				src={desktopView ? Grid : GridMobile}
+				src={tabletView ? Grid : GridMobile}
 				className='absolute -top-[41rem] left-1/2 z-0 w-[88rem] -translate-x-1/2 scale-[120%] select-none opacity-50 md:-top-80 md:scale-100 md:opacity-80'
 				alt='Grid background'
 				draggable='false'
 			/>
-			<About desktopView={desktopView} />
-			<Experience desktopView={desktopView} />
-			<Projects desktopView={desktopView} />
+			<About />
+			<Experience />
+			<Projects />
 			{/* TODO: Mobile-view */}
-			{desktopView && <Achievements desktopView={desktopView} />}
-			<Contact desktopView={desktopView} />
-			<Copyright desktopView={desktopView} />
+			{desktopView && <Achievements />}
+			<Contact />
+			<Copyright />
 		</div>
 	);
 };
