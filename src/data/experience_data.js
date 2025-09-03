aa31acf8d7ec2106f0cbe8a1d29ba296e5fae922
Daffa Abhipraya <daffaabhiprayaputra@gmail.com>
2025-09-03 14:08:31 +0700

    Batman

--- src/data/experience_data.js
+++ src/data/experience_data.js
@@ -16,7 +16,7 @@ const experienceData = [
     url: 'https://tekvision.co.id/',
     logo: TVI,
     date: '08/2025 - Present',
-    desc: '- Working on an AI-powered solutions for a banking industry client\n- Researching the feasibility of **photoplethysmogram (PPG)** for AI-based medical purposes',
+    desc: '- Working on an AI-powered solutions for an asurance industry client\n- Researching the feasibility of **photoplethysmogram (PPG)** for AI-based medical purposes',
   },
   {
     title: 'Product Engineer Intern',
@@ -33,7 +33,7 @@ const experienceData = [
     url: 'https://compfest.id/',
     logo: COMPFEST,
     date: '03/2025 - 09/2025',
-    desc: '- Currently managing COMPFEST CTF 2025 with 1900+ participants (~650 teams)—ongoing event\n- Led a 3-month architectural revamp and deployment of a CTF gameserver on Google Kubernetes Engine (GKE), secured with a VPN server, to support 45 finalists\n  - Backend: Flask, SocketIO, Redis, RabbitMQ, MySQL\n  - Frontend: Next.js, Tailwind CSS\n- Managed servers handling 500+ RPS with <100ms average response time via strategic GCP resource allocation\n- During COMPFEST 2024, designed and maintained a CTF platform hosting 1600+ participants (~550 teams) with 99.9% uptime during a 9-hour event',
+    desc: '- Currently managing COMPFEST CTF 2025 with 1900+ participants (~650 teams)—ongoing event\n- Led a 3-month architectural revamp and deployment of a CTF gameserver on Google Kubernetes Engine (GKE), secured with a VPN server, to support 45 finalists\n  - Backend: Flask, SocketIO, Redis, RabbitMQ, MySQL\n  - Frontend: Next.js, Tailwind CSS\n- Managed servers handling 500+ RPS with <100ms average response time via strategic GCP resource allocation\n- Mitigated a DDoS attack (~300k requests in 1 hour from rotating mobile proxies across regions) by tightening Nginx rate limiting and Cloudflare filtering, resulting in minimal platform downtime while keeping all challenge instances online\n- During COMPFEST 2024, designed and maintained a CTF platform hosting 1600+ participants (~550 teams) with 99.9% uptime during a 9-hour event',
     alignCenter: false,
     previousTitles: ['Staff of Web Infra.'],
     previousDates: ['03/2024 - 11/2024'],
